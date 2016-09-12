9831a888f6c6c7044d2ca337d079aaa320c6ff1f
Chu Yang <chuyang@syswin.com>
2016-09-12 16:16:56 +0800

    solve problem 1 and 2.

---
+++ projecteuler/e0001.py
@@ -0,0 +1,20 @@
+"""
+Multiples of 3 and 5
+Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The add of these multiples is 23.
+
+Find the add of all the multiples of 3 or 5 below 1000.
+"""
+
+
+def sum_by(step, stop=1000):
+    """
+    sum up arithmetic progression by step
+    :param step: arithmetic progression step
+    :return: sum of arithmetic progression
+    """
+    return sum(range(step, stop, step))
+
+print sum_by(3) + sum_by(5) - sum_by(15)  # 233168
