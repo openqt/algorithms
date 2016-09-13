79343666ffe875c9299d4ee8ac301c80fe13016d
Chu Yang <chuyang@syswin.com>
2016-09-13 15:16:58 +0800

    problem 4 and 5.

---
+++ projecteuler/e0004.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+"""
+Largest palindrome product
+Problem 4
+
+A palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is
+9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+
+def reverse(n):
+    val = 0
+    while n > 0:
+        val = val * 10 + n % 10
+        n /= 10
+    return val
+
+
+def is_palindrome(n):
+    """
+    n is palindrome
+    :param n: the number
+    :return: True for palindrome
+    """
+    return n == reverse(n)
+
+
+a = b = c = 0
+for i in range(999, 99, -1):
+    for j in range(999, i, -1):
+        val = i * j
+        if val < c:
+            break
+        if is_palindrome(val):
+            a, b, c = i, j, val
+
+print '%d x %d = %d' % (a, b, c)
---
+++ projecteuler/e0005.py
@@ -0,0 +1,39 @@
+"""
+Smallest multiple
+Problem 5
+
+2520 is the smallest number that can be divided by each of the numbers from 1
+to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+"""
+
+
+def GCD(a, b):
+    """
+    greatest common divisor (G.C.D.)
+    :param a: a
+    :param b: b
+    :return: GCD
+    """
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+
+def LCM(a, b):
+    """
+    lowest common multiple (L.C.M.)
+    :param a: a
+    :param b: b
+    :return: LCM
+    """
+    return a * b / GCD(a, b)
+
+
+print reduce(LCM, range(1, 11))  # 2520
+print reduce(LCM, range(1, 21))  # 232792560
