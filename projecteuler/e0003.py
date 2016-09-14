bbd99e9af2794562bc419ca8ddd5f7551978d03d
Chu Yang <chuyang@syswin.com>
2016-09-14 11:12:34 +0800

    improve The sieve of Eratosthenes.

--- projecteuler/e0003.py
+++ projecteuler/e0003.py
@@ -9,7 +9,7 @@ What is the largest prime factor of the number 600851475143 ?
 import math
 
 
-def prime_by(n, reverse=False):
+def prime_by_1(n, reverse=False):
     """
     prime sequence to n
     :param n: the maximum range
@@ -17,15 +17,46 @@ def prime_by(n, reverse=False):
     :return: prime sequence between [2, n]
     """
     marker = bytearray(n)
-    for i in range(2, int(math.sqrt(n))):
-        if marker[i] == 0:
-            for j in range(i * i, n, i):
+    for i in range(4, n, 2):  # mark even numbers > 2
+        marker[i] = 1
+
+    for i in range(3, int(math.sqrt(n)), 2):
+        if marker[i] == 0:  # prime
+            for j in range(i * i, n, 2 * i):
                 marker[j] = 1
 
     _r = range(len(marker) - 1, 1, -1) if reverse else range(2, len(marker))
     for i in _r:
         if marker[i] == 0:
             yield i
+
+
+def prime_by(n, reverse=False):
+    """
+    prime sequence to n
+    :param n: the maximum range
+    :param reverse: reverse prime sequence
+    :return: prime sequence between [2, n]
+    """
+    marker = bytearray(n / 2)
+
+    for i in range(3, int(math.sqrt(n) + 1), 2):
+        if marker[i / 2] == 0:  # prime
+            for j in range(i * i, n, 2 * i):
+                marker[j / 2] = 1
+
+    if not reverse:
+        yield 2
+        _range = range(1, len(marker))
+    else:
+        _range = range(len(marker) - 1, 0, -1)
+
+    for i in _range:
+        if marker[i] == 0:
+            yield 2 * i + 1
+
+    if reverse:
+        yield 2
 
 
 if __name__ == '__main__':
