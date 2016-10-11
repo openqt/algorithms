cc1e3c704bd44fe74aa4f6d472db42d93f44cad6
Chu Yang <chuyang@syswin.com>
2016-10-11 18:33:42 +0800

    solved pe 39.

---
+++ projecteuler/e0039_integer_right_triangles.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+"""
+Integer right triangles
+Problem 39
+
+If p is the perimeter of a right angle triangle with integral length sides,
+{a,b,c}, there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p ≤ 1000, is the number of solutions maximised?
+"""
+from __future__ import print_function
+
+
+def right_triangle_sides(perimeter):
+    sides = []
+    for a in range(1, perimeter // 3):
+        for b in range(a, perimeter):
+            c = perimeter - a - b
+            if c > a and c > b:
+                if c ** 2 == a ** 2 + b ** 2:
+                    sides.append((a, b, c))
+    return sides
+
+
+if __name__ == '__main__':
+    n = sides = 0
+    for i in range(1001):
+        t = len(right_triangle_sides(i))
+        if t > sides:
+            sides = t
+            n = i
+    print(n, right_triangle_sides(n))  # 840
