1a4c287db1c26b1129a6e6dc52664bbe28c83e48
Chu Yang <openqt@163.com>
2016-10-15 21:22:48 +0800

    solved pe 19.

--- projecteuler/e0019_counting_sundays.py
+++ projecteuler/e0019_counting_sundays.py
@@ -24,5 +24,17 @@ How many Sundays fell on the first of the month during the twentieth century
 from __future__ import print_function
 
 
+def is_leap_year(year):
+    return (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)
+
+
 if __name__ == '__main__':
-    raise NotImplementedError()
+    n = 0
+    total = 1  # 每个月第一天
+    for i in range(1900, 2001):
+        feb = 29 if is_leap_year(i) else 28
+        for j in [31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]:
+            total += j
+            if total % 7 == 0 and i > 1900:  # 1900年不计
+                n += 1
+    print(n)
