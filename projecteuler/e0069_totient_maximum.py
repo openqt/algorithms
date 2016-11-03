d9be461cfd2942acc7bef09c9a39d02c6ae6ea1e
Jack Chu <openqt@163.com>
2016-11-03 18:05:20 +0800

    solved pe 70.

--- projecteuler/e0069_totient_maximum.py
+++ projecteuler/e0069_totient_maximum.py
@@ -25,32 +25,34 @@ Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
 """
 from __future__ import print_function, division
 from e0003_largest_prime_factor import prime_sieve
-# from e0005_smallest_multiple import GCD
 from e0007_10001st_primes import prime
 
 
-def totient(n):
+def totient(n, *args):
     """relatively prime to n
 
-    欧拉函数有这么一个简单的计算公式:
-        ϕ(n)=n∏p|n (1−1/p)
-
-    一句话说，就是找出所有能被n整除的质数p，然后计算出1−1p的连乘，再乘以n就可以了
+    欧拉函数有这么一个简单的计算公式: ϕ(n)=n∏p|n (1−1/p)
+    一句话说，就是找出所有能被n整除的质数p，然后计算出1−1/p的连乘，再乘以n就可以了
+    :param n: the number
+    :return: phi
     """
-    # return sum(1 if GCD(n, i) == 1 else 0 for i in range(2, n-1)) + 2
-    vals = [i for i in prime_sieve(n) if n % i == 0]
-    return reduce(lambda x, y: x * (1 - 1 / y), vals, n) if vals else n - 1
+    if n <= 1:
+        return 1
+
+    vals = args if args else [i for i in prime_sieve(n) if n % i == 0]
+    return int(reduce(lambda x, y: x - x / y, vals, n)) if vals else n - 1
 
 
 if __name__ == '__main__':
-    # print(max((i/totient(i), i) for i in range(2, 1000000, 2)))
+    # print(max((i / totient(i), i) for i in range(2, 1000000, 2)))
+    # exit(0)
 
     """
     事实上，这道题有一个很容易想到的纯手算方法，从上面计算ϕ(n)的公式我们就可以得到:
         n/ϕ(n)=∏p|n p/(p−1)
 
     注意这里p是质数，而p/(p−1)>1，所以一个数n，含有的可以整除的质数越多，那么这个数的的
-    nϕ(n)就越大，所以可以得到2×3×5×7×11×13×17=510510<1,000,000 是最大的解！！
+    n/ϕ(n)就越大，所以可以得到2×3×5×7×11×13×17=510510<1,000,000 是最大的解！！
     """
     total = 1
     for i in prime():
---
+++ projecteuler/e0070_totient_permutation.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+"""
+Totient permutation
+Problem 70
+
+Euler's Totient function, φ(n) [sometimes called the phi function], is used to
+determine the number of positive numbers less than or equal to n which are
+relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than
+nine and relatively prime to nine, φ(9)=6.
+
+The number 1 is considered to be relatively prime to every positive number,
+so φ(1)=1.
+
+Interestingly, φ(87109)=79180, and it can be seen that 87109 is a permutation
+of 79180.
+
+Find the value of n, 1 < n < 10^7, for which φ(n) is a permutation of n and the
+ratio n/φ(n) produces a minimum.
+"""
+from __future__ import print_function, division
+from e0003_largest_prime_factor import prime_sieve
+from e0049_prime_permutations import seq_int
+from e0069_totient_maximum import totient
+
+
+def index_by_ceil(seq, ceil):
+    """return index below ceil
+
+    :param seq: list
+    :param ceil: the maximum value
+    :return: index
+    """
+    n = 0
+    for i in seq:
+        if i > ceil:
+            break
+        n += 1
+    return n
+
+
+if __name__ == '__main__':
+    CEIL = 10000000
+    caches = [i for i in prime_sieve(CEIL)]
+
+    pos = index_by_ceil(caches, CEIL ** .5)
+    val = (CEIL, CEIL)
+
+    for n, i in enumerate(caches[:pos]):
+        end = index_by_ceil(caches, CEIL / i)
+        for j in caches[n + 1:end]:
+            n = i * j
+            phi = totient(n, i, j)
+            if sorted(seq_int(phi)) == sorted(seq_int(n)):
+                val = min(val, (n / phi, n))
+    print(val)  # 8319823
