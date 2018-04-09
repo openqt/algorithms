9ed9a0a1f9862fb1362807c5f95e546444bc33b5
初阳 <chuyang@t2cloud.net>
2018-04-09 19:17:29 +0800

    solved lc289

--- leetcode/lc289.go
+++ leetcode/lc289.go
@@ -4,76 +4,109 @@ import (
 	"fmt"
 )
 
-/* 73. Set Matrix Zeroes
-https://leetcode.com/problems/set-matrix-zeroes/description/
+/* 289. Game of Life
+https://leetcode.com/problems/game-of-life/description/
 
-Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+According to the Wikipedia's article: "The Game of Life, also known simply as Life,
+is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0).
+Each cell interacts with its eight neighbors (horizontal, vertical, diagonal)
+using the following four rules (taken from the above Wikipedia article):
+
+	Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+	Any live cell with two or three live neighbors lives on to the next generation.
+	Any live cell with more than three live neighbors dies, as if by over-population..
+	Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+Write a function to compute the next state (after one update) of the board given its current state.
 
 Follow up:
-	Did you use extra space?
-	A straight forward solution using O(mn) space is probably a bad idea.
-	A simple improvement uses O(m + n) space, but still not the best solution.
-	Could you devise a constant space solution?
+	Could you solve it in-place? Remember that the board needs to be updated at the same time:
+	You cannot update some cells first and then use their updated values to update other cells.
+
+	In this question, we represent the board using a 2D array. In principle, the board is infinite,
+	which would cause problems when the active area encroaches the border of the array.
+	How would you address these problems?
 */
-func setZeroes(matrix [][]int) {
-	// 最简单的mark位置，就是第一行第一列。
-	// 用第一行来保存各个列的mark，用第一列来保存各个行的mark。
-	row0 := false
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
-			if matrix[i][j] == 0 {
-				if i == 0 {
-					row0 = true
-				} else {
-					matrix[i][0] = 0
+func gameOfLife(board [][]int) {
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			n := lives(board, i, j)
+			if board[i][j] == 0 {
+				if n == 3 {
+					board[i][j] += 10
 				}
-				matrix[0][j] = 0
+			} else {
+				if n == 2 || n == 3 {
+					board[i][j] += 10
+				}
 			}
 		}
 	}
 
-	for i := 1; i < len(matrix); i++ {
-		if matrix[i][0] == 0 {
-			setRow(matrix, i)
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			board[i][j] /= 10
 		}
 	}
-	for j := 1; j < len(matrix[0]); j++ {
-		if matrix[0][j] == 0 {
-			setCol(matrix, j)
+}
+
+func lives(board [][]int, row, col int) (total int) {
+	for i := max(0, row-1); i <= min(len(board)-1, row+1); i++ {
+		for j := max(0, col-1); j <= min(len(board[0])-1, col+1); j++ {
+			total += board[i][j] % 10
 		}
 	}
-
-	if matrix[0][0] == 0 {
-		setCol(matrix, 0)
-	}
-	if row0 {
-		setRow(matrix, 0)
-	}
+	return total - board[row][col]
 }
 
-func setRow(matrix [][]int, n int) {
-	for j := 0; j < len(matrix[0]); j++ {
-		matrix[n][j] = 0
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
+	return b
 }
 
-func setCol(matrix [][]int, n int) {
-	for i := 0; i < len(matrix); i++ {
-		matrix[i][n] = 0
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
+	return b
 }
+
+const (
+	DD = 0 // DEAD->DEAD
+	LD = 1 // LIVE->DEAD
+
+	DL = 10 // DEAD->LIVE
+	LL = 11 // LIVE->LIVE
+)
 
 // http://www.cnblogs.com/lupx/p/leetcode-73.html
 func main() {
 	m := [][]int{
-		{1, 3, 0, 4},
-		{1, 9, 7, 4},
-		{1, 2, 0, 4},
-		{0, 3, 0, 1},
+		{1, 1, 0, 1},
+		{1, 1, 1, 1},
+		{1, 0, 0, 1},
+		{0, 0, 0, 1},
 	}
-	setZeroes(m)
-
+	gameOfLife(m)
 	for _, i := range m {
 		fmt.Println(i)
 	}
+
+	m2 := [][]int{
+		{1},
+		{0},
+		{0},
+		{1},
+		{0},
+		{0},
+		{1},
+		{0},
+		{0},
+		{1},
+	}
+	gameOfLife(m2)
 }
